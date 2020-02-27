7a2f39ed7716343577b5b2a35b526ed5587d5041
tuankrn197 <vantuankrn197@gmail.com>
2020-02-27 16:03:42 +0700

    fix category22

--- database/migrations/2020_02_19_070326_create_task_table.php
+++ database/migrations/2020_02_19_070326_create_task_table.php
@@ -17,6 +17,10 @@ class CreateTaskTable extends Migration
             $table->bigIncrements('id');
             $table->string('title');
             $table->text('content');
+            $table->unsignedBigInteger('user_id');
+            $table->boolean('clip')->default(false);
+            $table->boolean('public');
+            $table->foreign('user_id')->references('id')->on('users')->onDelete('cascade')->onUpdate('cascade');
             $table->timestamps();
         });
     }
